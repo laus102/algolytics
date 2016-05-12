fc48e6b209cac7eaa5e3dfd00bea4bca0963d133
Brendan Lau <bjlau@umich.edu>
2016-05-12 14:05:06 -0400

    Initial Commit

---
+++ Algolytics/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Algolytics
+//
+//  Created by Brendan Lau on 5/12/16.
+//  Copyright © 2016 Incipia. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+   func applicationDidFinishLaunching(aNotification: NSNotification) {
+      // Insert code here to initialize your application
+   }
+
+   func applicationWillTerminate(aNotification: NSNotification) {
+      // Insert code here to tear down your application
+   }
+
+
+}
+
---
+++ Algolytics/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Algolytics
+//
+//  Created by Brendan Lau on 5/12/16.
+//  Copyright © 2016 Incipia. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+
+      // Do any additional setup after loading the view.
+   }
+
+   override var representedObject: AnyObject? {
+      didSet {
+      // Update the view, if already loaded.
+      }
+   }
+
+
+}
+
