d37656e113fb3c27fa65315f9a05d7c48b5f7a3c
Brendan Lau <bjlau@umich.edu>
2016-07-15 16:21:49 -0400

    test

---
+++ Algolytics/String_Extensions.swift
@@ -0,0 +1,42 @@
+//
+//  String_Extensions.swift
+//
+//  Created by Brendan Lau on 6/15/16.
+//  Copyright © 2016 Incipia. All rights reserved.
+//
+
+import Foundation
+
+private let kWhitespaceAndNewlineSet = CharacterSet.whitespacesAndNewlines
+private let kAlphaNumericSet = CharacterSet.alphanumerics
+
+extension String
+{
+   var trimmed: String {
+      return trimmingCharacters(in: kWhitespaceAndNewlineSet)
+   }
+   var words: [String] {
+   return self.components(separatedBy: " ")
+   }
+   var isValidEmail: Bool {
+      let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
+      let emailTest = Predicate(format:"SELF MATCHES %@", emailRegEx)
+      return emailTest.evaluate(with: self)
+   }
+//   var isValidSearchTerm: Bool {
+//      //let searchTermRegEx = ""
+//      //let searchTermTest =
+//   }
+   
+   func removeSpecialCharsFromString(text: String) -> String {
+      let okayChars : Set<Character> =
+         Set("abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLKMNOPQRSTUVWXYZ1234567890".characters)
+      return String(text.characters.filter {okayChars.contains($0) })
+   }
+   
+   func containsNoAlphaNumericCharacters() -> Bool {
+      let range = self.rangeOfCharacter(from: kAlphaNumericSet)
+      if let _ = range { return false }
+      return true
+   }
+}
